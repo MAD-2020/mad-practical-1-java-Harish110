faaf25ba4c4876cacf4dc880fc1b3c73bc25a300
Harish110 <64051688+Harish110@users.noreply.github.com>
2020-04-22 15:57:56 +0800

    Done QN 4

--- Question4.java
+++ Question4.java
@@ -18,6 +18,18 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter the base length");
+
+    int length = in.nextInt();
+
+    for (int a = base; a > 0; a--)
+    {
+        for(int b = 0; b < a; b++)
+        {
+            System.out.print("*")
+        }
+        System.out.println();
+    }
+
   }
 }
